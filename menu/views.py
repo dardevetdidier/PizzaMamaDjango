89241af32a2bcffa97950e39b9a10245b388fae0
Didier Dardevet <didier.dardevet@gmail.com>
2021-10-01 10:35:51 +0200

    Add Pizza Model. Add View, template, url Menu

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pizza
 
-# Create your views here.
+# / Menu
+def index(request):
+    '''pizzas = Pizza.objects.all()
+    pizzas_names_and_prices = [pizza.nom + " : " + str(pizza.prix) + "€" for pizza in pizzas]
+    pizzas_names_and_prices_str = ", ".join(pizzas_names_and_prices)
+    return HttpResponse(f"Les pizzas: {pizzas_names_and_prices_str}")'''
+
+    pizzas = Pizza.objects.all()
+
+
+    return render(request, 'menu/index.html', {'pizzas': pizzas})
