195ee5943d7e19c9ee9b90416bcee4270dbd3f98
Didier Dardevet <didier.dardevet@gmail.com>
2021-10-01 14:22:22 +0200

    menu ok. Main page ok.

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
 
-
-app_name = 'menu'
+app_name = 'main'
 
 urlpatterns = [
     path('', views.index, name="index"),
--- menu/views.py
+++ menu/views.py
@@ -9,7 +9,7 @@ def index(request):
     pizzas_names_and_prices_str = ", ".join(pizzas_names_and_prices)
     return HttpResponse(f"Les pizzas: {pizzas_names_and_prices_str}")'''
 
-    pizzas = Pizza.objects.all()
+    pizzas = Pizza.objects.all().order_by('prix')
 
 
     return render(request, 'menu/index.html', {'pizzas': pizzas})
