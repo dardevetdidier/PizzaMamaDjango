42ab0fd0c434dbfe5941e54a8d9ef61b5c30aa6f
Didier Dardevet <didier.dardevet@gmail.com>
2021-10-02 17:20:39 +0200

    Add contact page

--- PizzaMamaDjango/settings.py
+++ PizzaMamaDjango/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'menu',
     'main',
+    'contact',
 ]
 
 MIDDLEWARE = [
--- PizzaMamaDjango/urls.py
+++ PizzaMamaDjango/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('menu/', include('menu.urls')),
     path('', include('main.urls')),
+    path('', include('contact.urls')),
 ]
