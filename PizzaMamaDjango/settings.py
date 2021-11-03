cad681eedddf33aaebfd9afbca25e1200afc4660
Didier Dardevet <didier.dardevet@gmail.com>
2021-11-04 00:04:30 +0100

    modify static files dirs

--- PizzaMamaDjango/settings.py
+++ PizzaMamaDjango/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'PizzaMamaDjango.urls'
@@ -121,7 +122,9 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '/contact/static'),
+                    os.path.join(BASE_DIR, '/main/static'),
+                    os.path.join(BASE_DIR, '/menu/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
