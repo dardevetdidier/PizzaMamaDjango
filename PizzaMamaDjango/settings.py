4d1febe2a4f5759ccdd94fbb2d4514096d65ec12
Didier Dardevet <didier.dardevet@gmail.com>
2021-11-03 21:44:11 +0100

    change settings

--- PizzaMamaDjango/settings.py
+++ PizzaMamaDjango/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'tp3*2%0xw9!#i+77-kimw4-0obt+8(tb3!sdr9&zf%0xmkfo8k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
