2fc7e344f9e7b930356fc36ed3960c987fc25753
Didier Dardevet <didier.dardevet@gmail.com>
2021-11-04 15:35:29 +0100

    modify settings

--- PizzaMamaDjango/settings.py
+++ PizzaMamaDjango/settings.py
@@ -16,7 +16,7 @@ import django_heroku
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
