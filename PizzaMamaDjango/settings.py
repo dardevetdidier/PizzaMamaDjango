3cbb3e7f98aa239eb70a70efc400ca8436d44e51
Didier Dardevet <didier.dardevet@gmail.com>
2021-11-04 16:01:07 +0100

    modify settings debug=False

--- PizzaMamaDjango/settings.py
+++ PizzaMamaDjango/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 # SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'pizzamamapp.herokuapp.com']
 
